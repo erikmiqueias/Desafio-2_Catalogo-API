e76307f66556e3a774cf42b6fb5bf5ab9ca819f4
erikmiqueias <erikmiqueiaspereira@outlook.com>
2026-04-07 23:05:06 -0400

    chore: add tanstack query setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import "./index.css";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </QueryClientProvider>,
 );
